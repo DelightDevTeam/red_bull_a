f7da1dde6e6afe233a8d47cdb7d8bd0fd564ea4f
sophia <thangneimawi@aceplussolutions.com>
2024-08-29 22:26:26 +0700

    fixed bank seeder

--- database/seeders/PaymentTypeTableSeeder.php
+++ database/seeders/PaymentTypeTableSeeder.php
@@ -14,52 +14,48 @@ class PaymentTypeTableSeeder extends Seeder
     {
         $types = [
             [
-                'name' => 'Kasikorn Bank (K plus)',
-                'image' => 'KPLUS.png',
+                'name' => 'AYA Banking',
+                'image' => 'AYA banking.png',
             ],
             [
-                'name' => 'Siam Commercial Bank (SCB)',
-                'image' => 'SCB.png',
+                'name' => 'AYA Pay',
+                'image' => 'Aya pay.png',
             ],
             [
-                'name' => 'TMBTHANACHART BANK (TTB)',
-                'image' => 'TTB.png',
+                'name' => 'CB Banking',
+                'image' => 'CB banking.png',
             ],
             [
-                'name' => 'Krungthai Bank (KTB)',
-                'image' => 'KTB.png',
+                'name' => 'CB Pay',
+                'image' => 'CB pay logo.png',
             ],
             [
-                'name' => 'Bangkok Bank(BBL)',
-                'image' => 'BBL.png',
+                'name' => 'KBZ Banking',
+                'image' => 'KBZ banking.png',
             ],
             [
-                'name' => 'United Overseas Bank(UOB)',
-                'image' => 'UOB.png',
+                'name' => 'KBZ Pay',
+                'image' => 'Kpay.png',
             ],
             [
-                'name' => 'Kiatnakin Phatra Bank(KKP)',
-                'image' => 'KKP.png',
+                'name' => 'MAB Banking',
+                'image' => 'MAB banking.png',
             ],
             [
-                'name' => 'Government Savings Bank(GSB)',
-                'image' => 'GSB.png',
+                'name' => 'UAB Banking',
+                'image' => 'UAB banking.png',
             ],
             [
-                'name' => 'Bank of Ayudhya (BAY)',
-                'image' => 'BAY.png',
+                'name' => 'UAB Pay',
+                'image' => 'uab pay.png',
             ],
             [
-                'name' => 'Bank for Agriculture and Agricultural Cooperatives(BBAC)',
-                'image' => 'BBAC.png',
+                'name' => 'Wave Pay',
+                'image' => 'wave.png',
             ],
             [
-                'name' => 'Commerce International Merchant Bank(CIMB)',
-                'image' => 'CIMB.png',
-            ],
-            [
-                'name' => 'True Money Wallet',
-                'image' => 'truemoney.png',
+                'name' => 'Yoma Banking',
+                'image' => 'Yoma Banking.png',
             ],
         ];
 
