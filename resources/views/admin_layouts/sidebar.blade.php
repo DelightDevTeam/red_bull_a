4a5bf4389dc42a068aa05c8f2a1ff60d2ac1df96
KaungSettThu1873 <168012673+KaungSettThu1873@users.noreply.github.com>
2024-08-29 18:25:23 +0630

    Update sidebar.blade.php

--- resources/views/admin_layouts/sidebar.blade.php
+++ resources/views/admin_layouts/sidebar.blade.php
@@ -6,9 +6,9 @@
       <a class="nav-link text-white " href="{{ route('home') }}" style="font-szie:large;">
         <span class="sidenav-mini-icon"> <i class="material-icons-round opacity-10">dashboard</i> </span>
         @if(Auth::user()->hasRole('Admin'))
-        <span class="sidenav-normal ms-2 ps-1">Admin Dashboard</span>
+        <span class="sidenav-normal ms-2 ps-1">@lang('public.admin_dashboard')</span>
         @elseif(Auth::user()->hasRole('Agent'))
-        <span class="sidenav-normal ms-2 ps-1">Agent Dashboard</span>
+        <span class="sidenav-normal ms-2 ps-1">@lang('public.agent_dashboard')</span>
         @endif
       </a>
     </li>
