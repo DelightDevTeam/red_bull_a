9609e319155c62f4cdf15298cf317f71a26c1d94
sophia <thangneimawi@aceplussolutions.com>
2024-08-29 23:08:22 +0700

    fixed withdraw

--- app/Http/Controllers/Admin/WithDraw/WithDrawRequestController.php
+++ app/Http/Controllers/Admin/WithDraw/WithDrawRequestController.php
@@ -41,7 +41,7 @@ class WithDrawRequestController extends Controller
             ]);
 
             if ($request->status == 1) {
-                app(WalletService::class)->transfer($agent, $player, $request->amount, TransactionName::DebitTransfer);
+                app(WalletService::class)->transfer($player, $agent, $request->amount, TransactionName::DebitTransfer);
             }
 
             return redirect()->route('admin.agent.withdraw')->with('success', 'Withdraw status updated successfully!');
